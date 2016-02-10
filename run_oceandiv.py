658568c0d546a9281f193b5142e7d223a00a9e33
cdr30 <roberts.cd@gmail.com>
2016-02-10 18:11:42 +0000

    Renamed script

--- run_oceandiv.py
+++ run_oceandiv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python2.7
 """
-Script to execute TransConv from the command line.
+Script to execute OceanDiv from the command line.
 
 """
 
 import sys
 
-from transconv.transconv import main
+from oceandiv.oceandiv import main
 
 
 if __name__ == '__main__':
